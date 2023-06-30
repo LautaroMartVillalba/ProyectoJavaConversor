5d6c649af262bf8577a2abae0a89590a3bd8ee88
Lautaro <lautaromartinvillalba@gmail.com>
2023-06-29 23:56:06 -0300

    Ventana creada

--- src/graficos/CreandoMarcos.java
+++ src/graficos/CreandoMarcos.java
@@ -1,10 +1,24 @@
 package graficos;
 
+import javax.swing.*;
+
 public class CreandoMarcos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		miMarco primerMarco = new miMarco();
+		
+			
+		
+	}
+		
 	}
 
+class miMarco extends JFrame{
+	
+	public miMarco() {
+		setSize(700, 600);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
