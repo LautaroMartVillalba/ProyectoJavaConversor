47361830eb0cc429ad3b7cceb44f116cd61bf395
Lautaro <lautaromartinvillalba@gmail.com>
2023-07-02 20:55:55 -0300

    Creando fondo experimental, ordenando código y mejorando explicciones de código no util

--- src/graficos/CreandoMarcos.java
+++ src/graficos/CreandoMarcos.java
@@ -2,6 +2,9 @@ package graficos;
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.geom.*;
+import javax.imageio.*;
+import java.io.*;
 
 public class CreandoMarcos {
 
@@ -9,12 +12,10 @@ public class CreandoMarcos {
 		
 		MiMarco conversor = new MiMarco();
 		
-		conversor.setVisible(true);
 		conversor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//EXIT_ON_CLOSE hace que el programa finalice al cerrarse
 		conversor.setTitle("Conversor");
 		
-		
-		}
+	}
 }
 
 
@@ -22,7 +23,56 @@ class MiMarco extends JFrame{
 	
 	public MiMarco() {
 		Toolkit mipantalla=Toolkit.getDefaultToolkit();
-		Dimension tamanhoPantalla = mipantalla.getScreenSize();//crea un nuevo objeto de dimensión, y al anterior objeto de Toolkit se encarga de capturar los datos del tamaño de la pantalla del dispositivo
+		Dimension tamanhoPantalla = mipantalla.getScreenSize();
+		setResizable(false);
+		int altura = tamanhoPantalla.height;
+		int anchura = tamanhoPantalla.width;
+		setSize(anchura / 2, altura / 2);
+		setVisible(true);
+		setLocation(anchura/4, altura / 4);
+		Image icono = mipantalla.getImage("src/graficos/icono.jpg");
+		setIconImage(icono);
+		Panel miPanel = new Panel();
+		add(miPanel);
+	}
+}
+
+
+
+class Panel extends JPanel{ 
+	
+	public void paintComponent(Graphics g) { 
+		super.paintComponent(g); 
+		
+		Graphics2D g2 = (Graphics2D) g;
+		
+		//File miImagen = new File("src/graficos/fondo1.jpg");//desde aca
+		try {
+			imagen = ImageIO.read(new File("src/graficos/fondo1.jpg"));
+		} catch (IOException e) {
+			System.out.println("No está la imagen, pibe");
+		} //hasta acá, sólo se crea e instancia una imagen
+		g2.drawImage(imagen, 0, 0, null); //acá se inserta la imagen
+		Rectangle2D rectangulo = new Rectangle2D.Double(100, 100, 200, 200);
+		g2.setPaint(Color.BLUE); 
+		g2.fill(rectangulo); 
+		g2.draw(rectangulo); 
+		
+		Font letra = new Font("Arial", Font.ITALIC, 30);
+		g2.setColor(Color.ORANGE);
+		g2.setFont(letra);
+		g2.drawString("Hola, buenas:D", 50, 25);
+	}
+	private Image imagen;
+}
+
+//A partir de acá se encuentran apuntes de aprendizaje no utilizados en el proyecto, o que carecen de utilidad por ahora
+
+class MiMarcoBeta extends JFrame{
+	
+	/*public MiMarco() {
+		Toolkit mipantalla=Toolkit.getDefaultToolkit();
+		Dimension tamanhoPantalla = mipantalla.getScreenSize();//crea un nuevo objeto de dimensión, y al anterior objeto de Toolkit se encarga de capturar los 	datos del tamaño de la pantalla del dispositivo
 		//setBounds(300, 100, 700, 600); //Es setSize y setLocation al mismo tiempo
 		setResizable(false); //Impide que el marco se pueda redimensionar
 		int altura = tamanhoPantalla.height; //logran darle "cuerpo" a los valores de altura y anchura capturados por getScreenSize()
@@ -30,19 +80,36 @@ class MiMarco extends JFrame{
 		setSize(anchura / 2, altura / 2);
 		setVisible(true);
 		setLocation(anchura/4, altura / 4);
-		Image icono = mipantalla.getImage("C:\\Users\\lauta\\Documents\\ProyectoConversor\\Conversor.java\\Imagenes\\icono.jpg");
+		Image icono = mipantalla.getImage("src/graficos/icono.jpg");
 		setIconImage(icono);
 		Panel miPanel = new Panel();
 		add(miPanel);
-		
-	}
+		miPanel.setBackground(Color.RED);
+		miPanel.setForeground(Color.YELLOW);
+	}*/
 }
 
-class Panel extends JPanel{ //Crea una nueva clase panel, que tiene recrea el método paintComponent
+class PanelBeta extends JPanel{
 	
-	public void paintComponent(Graphics g) { //reescribe el método
-		super.paintComponent(g); //Llama a que el método cumpla su funcionamiento natural
-		g.drawString("Hola, Mundo!:D", 50, 100); //Crea un String con coordenadas 50 100
+	/*public void paintComponent(Graphics g) {
+		super.paintComponent(g); Llama a que el método cumpla su funcionamiento natural
+		
+		Graphics2D g2 = (Graphics2D) g;
+		Rectangle2D rectangulo = new Rectangle2D.Double(100, 100, 200, 200);
+		g2.setPaint(Color.BLUE); Lo que hace es "tomar" el color con lo que va a pintar lo siguiente
+		g2.fill(rectangulo); fill dibuja el contorno del rectángulo según el color seleccionado
+		g2.draw(rectangulo); draw rellena la figura con elcolor selecionado
+		
+		Ellipse2D elipse = new Ellipse2D.Double();
+		elipse.setFrame(rectangulo);
+		g2.setPaint(new Color(52,18,115)); new Color(...) utiliza el sistema RGB para crear un nuevo color
+		g2.fill(elipse);
+		g2.draw(elipse);
+		g2.draw(new Line2D.Double(10, 10, 50, 90)); Dibuja una linea
+		
+		Font letra = new Font("Arial", Font.ITALIC, 30); Crea un nuevo tpo de letra, con el tipo de fuente, el estilo, y su tamaño
+		g2.setColor(Color.ORANGE);
+		g2.setFont(letra);
+		g2.drawString("Hola, buenas:D", 50, 25);*/
+		
 	}
-	
-}
